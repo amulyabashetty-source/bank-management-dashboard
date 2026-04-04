683c99e2091aa709613ed21b4d45755de3dc9e7a
Amulya <amulyabashetty@gmail.com>
2026-04-05 00:57:41 +0530

    fixed DB connection for production

--- backend/db.py
+++ backend/db.py
@@ -3,9 +3,9 @@ import os
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT"))
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT"))
     )
