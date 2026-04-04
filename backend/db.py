555febd33126cadc415a3bdcde982ec7693925fc
Amulya <amulyabashetty@gmail.com>
2026-04-05 00:50:41 +0530

    fixed db.py file

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,11 @@
 import mysql.connector
+import os
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="your_password",
-        database="BANK_PDBC"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
     )
